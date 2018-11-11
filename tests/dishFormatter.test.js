02fdcec337a06b333088d9b553e1a450a660ca12
timothy.hawkins <timothy.hawkins@kurtgeiger.com>
2018-11-11 15:42:19 +0000

    adds full test coverage for dishFormatter

--- tests/dishFormatter.test.js
+++ tests/dishFormatter.test.js
@@ -4,6 +4,19 @@ const restaurantMock = require('./mocks/restaurantMock');
 describe('dishFormatter', () => {
   describe('run()', () => {
     it('should return the outcome of loopDishes()', () => {
+      const result = dishFormatter.run(restaurantMock);
+      expect(result[0]).toEqual({ 
+        name: [ 'American', 'Hot' ],
+        description:
+        [ 'Pepperoni,',
+          'jalapeno',
+          'peppers,',
+          'onions',
+          '&',
+          'mixed',
+          'peppers' ],
+        price: '£11.99' 
+      });
     });
   });
 
@@ -29,6 +42,12 @@ describe('dishFormatter', () => {
     it('should convert given string to array()', () => {
       const expected = ['Testing', 'String', 'To', 'Array'];
       const actual = dishFormatter.stringToArray('Testing String To Array');
+      expect(expected).toEqual(actual);
+    });
+
+    it('returns null if given string is null', () => {
+      const expected = null;
+      const actual = dishFormatter.stringToArray(null);
       expect(expected).toEqual(actual);
     });
   });
